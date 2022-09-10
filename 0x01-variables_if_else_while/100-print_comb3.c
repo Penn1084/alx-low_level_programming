b414b12f7b0f990bc84d9a14e172ac504eae135a
Penn1084 <arc1084penn@gmail.com>
2022-09-10 06:36:57 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,65 @@
-/*
- * File: 100-print_comb3.c
- * Auth: penn1084
- */
+nclude <stdio.h>
 
-#include <stdio.h>
+
 
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0.
- */
+ *   * main - Prints combination of numbers
+ *
+ *     *
+ *
+ *       * Return: Always (Success)
+ *
+ *         */
+
 int main(void)
+
 {
-	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+		int c, i;
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
-		}
-	}
 
-	putchar('\n');
 
-	return (0);
+			for (c = '0'; c <= '9'; c++)
+
+					{
+
+								for (i = '0'; i <= '9'; i++)
+
+											{
+
+															if (c < i)
+
+																			{
+
+																								putchar(c);
+
+																												putchar(i);
+
+
+
+																																if (c != '8' || (c == '8' && i != '9'))
+
+																																					{
+
+																																											putchar(',');
+
+																																																putchar(' ');
+
+																																																				}
+
+																																			}
+
+																	}
+
+									}
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
 }
