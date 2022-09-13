9e6c9a4f7fb176466904889892a74a4151204d7f
Penn1084 <arc1084penn@gmail.com>
2022-09-13 12:40:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - putchar
+* Return: 0
+* Auth: Muhammad Abubakar
+*/
+
+int main(void)
+{
+      _putchar('_');
+      _putchar('p');
+      _putchar('u');
+      _putchar('t');
+      _putchar('c');
+      _putchar('h');
+      _putchar('a');
+      _putchar('r');l
+      _putchar('\n');
+      
+      return (o);
+    
+    
+    }
