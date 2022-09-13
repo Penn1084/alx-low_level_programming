8033c77ff7bed2d353ba3074d00fafda1de4df2d
Penn1084 <arc1084penn@gmail.com>
2022-09-13 12:52:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,10 +15,10 @@ int main(void)
       _putchar('c');
       _putchar('h');
       _putchar('a');
-      _putchar('r');l
+      _putchar('r');
       _putchar('\n');
       
-      return (o);
+      return (0);
     
     
     }
