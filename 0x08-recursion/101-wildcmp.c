53d2d1ec9e0d5c84d5b2c50fee52629e849ac141
Penn1084 <arc1084penn@gmail.com>
2022-09-28 10:25:42 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,22 @@
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int _putchar.c(char c);
+/**
+ * wildcmp - Compare two strings allowing for wildcard char
+ * @s1: String being compared
+ * @s2: String being compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
