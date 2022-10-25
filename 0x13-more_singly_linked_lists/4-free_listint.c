59781ad39c850bf879179d1892102d76db22641d
Penn1084 <arc1084penn@gmail.com>
2022-10-25 07:23:51 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,13 @@
 #include "lists.h"
+
 /**
- * free_listint - Entry Point
- * @head: head
- * Return: 0
+ * free_listint - Free listint_t.
+ * @head: data type pointer the head/next node
+ * Return: 0 elements all free
  */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-		return;
-
-	while (head)
+	while (head != NULL)
 	{
 		free(head);
 		head = head->next;
