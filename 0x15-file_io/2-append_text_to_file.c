1cbeb8937482535f8a798cd032c6c539073233a8
Penn1084 <arc1084penn@gmail.com>
2022-11-02 05:34:58 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,42 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Entry Point
- * @filename: file name
- * @text_content: text content
- * Return: 1
+ * append_text_to_file - a funcion that appends text at the end
+ *                      of a file
+ *
+ * @filename: file to add data to
+ * @text_content: text content to add to file
+ *
+ * Return: 1 on success, -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int file, app_status, words = 0;
 
-	if (filename == NULL)
+
+	if (filename == NULL) /*check if file is present*/
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
+	/*open file, with append option with write rights*/
+
+	file = open(filename, O_APPEND | O_WRONLY);
+
+	if (file == -1) /*check if file is present*/
 		return (-1);
 
-
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	if (text_content) /*append content to file if its not NULL*/
 	{
-		close(file);
-		return (1);
-	}
-	else
-	{
-		wr = write(file, text_content, i);
-	}
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
 
-	if (wr == -1)
-		return (-1);
+		/*append to file*/
 
-	close(file);
+		app_status = write(file, text_content, words);
+
+		if (app_status == -1) /*check if append was a success*/
+			return (-1);
+	}
+	close(file); /*close file*/
 	return (1);
 }
